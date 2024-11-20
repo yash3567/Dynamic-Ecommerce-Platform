09d273a50358c3a4b049ecdee2afcb743d6d2966
yash3567 <yashnandeshwar32@gmail.com>
2024-11-20 21:09:26 +0530

    Day 20

--- frontend/src/pages/Details/index.jsx
+++ frontend/src/pages/Details/index.jsx
@@ -25,22 +25,41 @@ const DetailsPage = () => {
     const [inputValue, setInputValue] = useState(1)
 
 
+    const zoomSliderBig = useRef()
     const zoomSlider = useRef()
+
+    var settings2 = {
+        dots: false,
+        infinite: false,
+        speed: 700,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: false,
+        arrows: false,
+
+    };
+
+
+
+
 
 
     var settings = {
         dots: false,
         infinite: false,
         speed: 500,
-        slidesToShow: 4,
+        slidesToShow: 5,
         slidesToScroll: 1,
         fade: false,
         arrows: true,
         // autoplay: 2000
     };
 
-    const goto = (url) => {
-        setZommImage(url)
+    const goto = (index) => {
+        // setZommImage(url)
+        zoomSlider.current.slickGoTo(index)
+        zoomSliderBig.current.slickGoTo(index)
+
 
 
     }
@@ -76,124 +95,146 @@ const DetailsPage = () => {
                 </div>
 
             </div>
-            <div className="container-fluid detailsContainer" style={{ padding: "0px 42px" }}>
-                <div className="row pt-3 pb-3">
-                    <div className="col-md-9 leftPart">
-                        <div className="row">
 
 
 
+            <div className="container-fluid detailsContainer pt-3 pb-3">
+                {/* style={{ padding: "0px 42px" }} */}
+                <div className="row">
 
 
+                    {/* productzoom  code start here  */}
 
-
-
-
-                            {/* productzoom  code start here  */}
-
-                            <div className="col-md-5 ">
-                                <div className="productZoom">
+                    <div className="col-md-5 ">
+                        <div className="productZoom">
+                            <Slider {...settings2} className="zoomSliderBig" ref={zoomSliderBig}>
+                                <div className="item">
                                     <InnerImageZoom zoomType='hover' zoomScale={1}
-                                        src={`${zoomImage}?im=Resize=(${bigImageSize[0]},${bigImageSize[1]})`} />
+                                        src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-0-202305292130.jpg?im=Resize=(${bigImageSize[0]},${bigImageSize[1]})`} />
                                 </div>
 
+                                <div className="item">
+                                    <InnerImageZoom zoomType='hover' zoomScale={1}
+                                        src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-1-202305292130.jpg?im=Resize=(${bigImageSize[0]},${bigImageSize[1]})`} />
+                                </div>
+                                <div className="item">
+                                    <InnerImageZoom zoomType='hover' zoomScale={1}
+                                        src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-2-202305292130.jpg?im=Resize=(${bigImageSize[0]},${bigImageSize[1]})`} />
+                                </div>
+                                <div className="item">
+                                    <InnerImageZoom zoomType='hover' zoomScale={1}
+                                        src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-3-202305292130.jpg?im=Resize=(${bigImageSize[0]},${bigImageSize[1]})`} />
+                                </div>
+                                <div className="item">
+                                    <InnerImageZoom zoomType='hover' zoomScale={1}
+                                        src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-legal-images-o490000363-p490000363-4-202305292130.jpg?im=Resize=(${bigImageSize[0]},${bigImageSize[1]})`} />
+                                </div>
+                                <div className="item">
+                                    <InnerImageZoom zoomType='hover' zoomScale={1}
+                                        src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-legal-images-o490000363-p490000363-5-202305292130.jpg?im=Resize=(${bigImageSize[0]},${bigImageSize[1]})`} />
+                                </div>
+                            </Slider>
+                        </div>
 
 
-                                <Slider {...settings} className="zoomSlider" ref={zoomSlider}>
-                                    <div className="item">
-                                        <img src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-0-202305292130.jpg?im=Resize=(${smlImageSize[0]},${smlImageSize[1]})`} alt="hey yash image not found" className="w-100"
-                                            onClick={() => goto('https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-0-202305292130.jpg')} />
-                                    </div>
-                                    <div className="item">
-                                        <img src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-1-202305292130.jpg?im=Resize=(${smlImageSize[0]},${smlImageSize[1]})`} alt="hey yash image not found" className="w-100"
-                                            onClick={() => goto('https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-1-202305292130.jpg')} />
-                                    </div>
-                                    <div className="item">
-                                        <img src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-2-202305292130.jpg?im=Resize=(${smlImageSize[0]},${smlImageSize[1]})`} alt="hey yash image not found" className="w-100"
-                                            onClick={() => goto('https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-2-202305292130.jpg')} />
-                                    </div>
-                                    <div className="item">
-                                        <img src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-3-202305292130.jpg?im=Resize=(${smlImageSize[0]},${smlImageSize[1]})`} alt="hey yash image not found" className="w-100"
-                                            onClick={() => goto('https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-3-202305292130.jpg')} />
-                                    </div>
-                                    <div className="item">
-                                        <img src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-legal-images-o490000363-p490000363-4-202305292130.jpg?im=Resize=(${smlImageSize[0]},${smlImageSize[1]})`} alt="hey yash image not found" className="w-100"
-                                            onClick={() => goto('https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-legal-images-o490000363-p490000363-4-202305292130.jpg')} />
-                                    </div>
-                                    <div className="item">
-                                        <img src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-legal-images-o490000363-p490000363-5-202305292130.jpg?im=Resize=(${smlImageSize[0]},${smlImageSize[1]})`} alt="hey yash image not found" className="w-100"
-                                            onClick={() => goto('https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-legal-images-o490000363-p490000363-5-202305292130.jpg')} />
-                                    </div>
-                                </Slider>
+
+                        <Slider {...settings} className="zoomSlider" ref={zoomSlider}>
+                            <div className="item">
+                                <img src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-0-202305292130.jpg?im=Resize=(${smlImageSize[0]},${smlImageSize[1]})`} alt="hey yash image not found" className="w-100"
+                                    onClick={() => goto(0)} />
                             </div>
-                            {/* productzoom code  end here */}
-
-
-
-                            {/* product information here  */}
-                            <div className="col-md-7 productInfo">
-                                <h1>Seeds of Change <br /> Organic Quinoa,<br /> Brown</h1>
-                                <div className="d-flex align-items-center mb-4" style={{ marginTop: 3 }}>
-                                    <Rating name="half-rating-read" defaultValue={4.5} precision={0.5} readOnly />
-                                    <span className="text-Light" style={{ marginLeft: '5px' }}>(32 reviews)</span>
-                                </div>
-
-
-                                <div className="priceSec d-flex align-items-center mb-3">
-                                    <span className="text-g priceLarge">$38</span>
-                                    <div className="d-flex flex-column" style={{ marginLeft: 3 }}>
-                                        <span className="text-org off">26% Off</span>
-                                        <span className="text-Light oldPrice">$52</span>
-                                    </div>
-                                </div>
-                                <p style={{ fontSize: '19px' }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />
-                                    Aliquam rem officia, corrupti reiciendis minima nisi modi,<br />
-                                    quasi, odio minus dolore impedit fuga eum eligendi.</p>
-
-
-
-
-                                <div className="productSize d-flex align-items-center">
-                                    <span style={{ opacity: '0.5', fontWeight: 'bold' }}>Size / Weight:</span>
-                                    <ul className="list list-inline mb-0" style={{ paddingLeft: '20px' }} >
-                                        <li className="list-inline-item"><a className={`tag ${activeSize === 0 ? 'active' : ''}`} onClick={() => isActive(0)}>50g</a></li>
-                                        <li className="list-inline-item"><a className={`tag ${activeSize === 1 ? 'active' : ''}`} onClick={() => isActive(1)}>80g</a></li>
-                                        <li className="list-inline-item"><a className={`tag ${activeSize === 2 ? 'active' : ''}`} onClick={() => isActive(2)}>60g</a></li>
-                                        <li className="list-inline-item"><a className={`tag ${activeSize === 3 ? 'active' : ''}`} onClick={() => isActive(3)}>100g</a></li>
-                                        <li className="list-inline-item"><a className={`tag ${activeSize === 4 ? 'active' : ''}`} onClick={() => isActive(4)}>150g</a></li>
-                                    </ul>
-                                </div>
-
-
-
-                                <div className="addCartSection d-flex align-items-center" style={{ paddingTop: 4, paddingBottom: 4, }}>
-                                    <div className="counterSec" style={{ marginRight: '15px' }}>
-                                        <input type="number" value={inputValue} />
-                                        <span className=" arrow plus" onClick={plus}><KeyboardArrowUpOutlinedIcon /></span>
-                                        <span className=" arrow minus" onClick={minus}><KeyboardArrowDownOutlinedIcon /></span>
-                                    </div>
-
-
-                                    <Button className="btn-g btn-lg addtocartbtn"><ShoppingCartOutlinedIcon style={{ marginRight: '2px' }} />Add to Cart</Button>
-                                    <Button className="move btn-lg addtocartbtn btn-border" style={{ marginLeft: '10px' }} ><FavoriteBorderOutlinedIcon /></Button>
-                                    <Button className="move btn-lg addtocartbtn btn-border" style={{ marginLeft: '10px' }}><CompareArrowsOutlinedIcon /></Button>
-                                </div>
+                            <div className="item">
+                                <img src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-1-202305292130.jpg?im=Resize=(${smlImageSize[0]},${smlImageSize[1]})`} alt="hey yash image not found" className="w-100"
+                                    onClick={() => goto(1)} />
                             </div>
-                            {/* productinfo end here  */}
+                            <div className="item">
+                                <img src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-2-202305292130.jpg?im=Resize=(${smlImageSize[0]},${smlImageSize[1]})`} alt="hey yash image not found" className="w-100"
+                                    onClick={() => goto(2)} />
+                            </div>
+                            <div className="item">
+                                <img src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-product-images-o490000363-p490000363-3-202305292130.jpg?im=Resize=(${smlImageSize[0]},${smlImageSize[1]})`} alt="hey yash image not found" className="w-100"
+                                    onClick={() => goto(3)} />
+                            </div>
+                            <div className="item">
+                                <img src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-legal-images-o490000363-p490000363-4-202305292130.jpg?im=Resize=(${smlImageSize[0]},${smlImageSize[1]})`} alt="hey yash image not found" className="w-100"
+                                    onClick={() => goto(4)} />
+                            </div>
+                            <div className="item">
+                                <img src={`https://www.jiomart.com/images/product/original/490000363/maggi-2-minute-masala-noodles-70-g-legal-images-o490000363-p490000363-5-202305292130.jpg?im=Resize=(${smlImageSize[0]},${smlImageSize[1]})`} alt="hey yash image not found" className="w-100"
+                                    onClick={() => goto(5)} />
+                            </div>
+                        </Slider>
+                    </div>
+                    {/* productzoom code  end here */}
 
+
+
+                    {/* product information here  */}
+                    <div className="col-md-7 productInfo">
+                        <h1>Seeds of Change Organic Quinoa, Brown</h1>
+                        <div className="d-flex align-items-center mb-4" style={{ marginTop: 3 }}>
+                            <Rating name="half-rating-read" defaultValue={4.5} precision={0.5} readOnly />
+                            <span className="text-Light" style={{ marginLeft: '5px' }}>(32 reviews)</span>
+                        </div>
+
+
+                        <div className="priceSec d-flex align-items-center mb-3">
+                            <span className="text-g priceLarge">$38</span>
+                            <div className="d-flex flex-column" style={{ marginLeft: 3 }}>
+                                <span className="text-org off">26% Off</span>
+                                <span className="text-Light oldPrice">$52</span>
+                            </div>
+                        </div>
+                        <p style={{ fontSize: '19px' }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                            Aliquam rem officia, corrupti reiciendis minima nisi modi,
+                            quasi, odio minus dolore impedit fuga eum eligendi.</p>
+
+
+
+
+                        <div className="productSize d-flex align-items-center">
+                            <span style={{ opacity: '0.5', fontWeight: 'bold' }}>Size / Weight:</span>
+                            <ul className="list list-inline mb-0" style={{ paddingLeft: '20px' }} >
+                                <li className="list-inline-item"><a className={`tag ${activeSize === 0 ? 'active' : ''}`} onClick={() => isActive(0)}>50g</a></li>
+                                <li className="list-inline-item"><a className={`tag ${activeSize === 1 ? 'active' : ''}`} onClick={() => isActive(1)}>80g</a></li>
+                                <li className="list-inline-item"><a className={`tag ${activeSize === 2 ? 'active' : ''}`} onClick={() => isActive(2)}>60g</a></li>
+                                <li className="list-inline-item"><a className={`tag ${activeSize === 3 ? 'active' : ''}`} onClick={() => isActive(3)}>100g</a></li>
+                                <li className="list-inline-item"><a className={`tag ${activeSize === 4 ? 'active' : ''}`} onClick={() => isActive(4)}>150g</a></li>
+                            </ul>
+                        </div>
+
+
+
+                        <div className="addCartSection d-flex align-items-center" style={{ paddingTop: 4, paddingBottom: 4, }}>
+                            <div className="counterSec" style={{ marginRight: '15px' }}>
+                                <input type="number" value={inputValue} />
+                                <span className=" arrow plus" onClick={plus}><KeyboardArrowUpOutlinedIcon /></span>
+                                <span className=" arrow minus" onClick={minus}><KeyboardArrowDownOutlinedIcon /></span>
+                            </div>
+
+
+                            <Button className="btn-g btn-lg addtocartbtn"><ShoppingCartOutlinedIcon style={{ marginRight: '2px' }} />Add to Cart</Button>
+                            <Button className="move btn-lg addtocartbtn btn-border" style={{ marginLeft: '10px' }} ><FavoriteBorderOutlinedIcon /></Button>
+                            <Button className="move btn-lg addtocartbtn btn-border" style={{ marginLeft: '10px' }}><CompareArrowsOutlinedIcon /></Button>
                         </div>
                     </div>
 
 
-                    <div className="col-md-3 rightPart sidebarWrapper">
-                        <Sidebar />
 
-                    </div>
+                    {/* product info ends here  */}
+
                 </div>
             </div>
         </section>
     )
 }
+
+
+{/* 
+                    <div className="col-md-3 rightPart sidebarWrapper">
+                        <Sidebar />
+
+                    </div> */}
 
 export default DetailsPage
 
